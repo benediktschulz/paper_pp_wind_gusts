7aa4a2f9f69dd4474d7291f40213f73e8ccc4a0c
Benedikt Schulz <56957602+benediktschulz@users.noreply.github.com>
2021-10-27 09:08:55 +0200

    Remove error

--- example_drn.R
+++ example_drn.R
@@ -10,23 +10,29 @@ i_valid <- 800:984
 # Import postprocessing function
 source(paste0(getwd(), "/pp_drn.R"))
 
+# Use variables besides observations and individual ensemble members as predictors
+pred_vars <- names(df_train)[!is.element(names(df_train), c("obs", paste0("ens_", 1:20)))]
+
 # Apply DRN function
 pred <- drn_pp(train = df_train, 
                X = df_test, 
                i_valid = i_valid, 
                loc_id_vec = paste0(1:10),
-               pred_vars = names(df_train)[names(df_train) != "obs"],
+               pred_vars = pred_vars,
                nn_ls = list(n_sim = 3,
                             nn_verbose = TRUE))
 
-# Take a look at forecasts
-summary(pred[["f"]])
+# Take a look at some forecasts
+head(pred[["f"]])
 
 # Take a look at evaluation measures of postprocessed forecasts
 summary(pred[["scores_pp"]])
 
 # PIT histogram
-hist(x = pred[["scores_pp"]][["pit"]])
+hist(x = pred[["scores_pp"]][["pit"]],
+     freq = FALSE)
+abline(h = 1,
+       lty = 2)
 
-# Calculate CRPSS
+# Calculate CRPSS w.r.t. ensemble forecasts (in %)
 print(100*(1 - mean(pred[["scores_pp"]][["crps"]])/mean(pred[["scores_ens"]][["crps"]])))
